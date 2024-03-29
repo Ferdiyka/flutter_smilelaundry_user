534ca25c6a0cd749430541eb308f8cd06c3b2421
Andyka Putra <andykaputra219@gmail.com>
2024-03-29 14:46:51 +0700

    first commit

---
+++ lib/presentation/intro/splash_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../core/assets/assets.gen.dart';
+import '../../core/constants/colors.dart';
+import '../../core/router/app_router.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => context.goNamed(
+        RouteConstants.root,
+        pathParameters: PathParameters().toMap(),
+      ),
+    );
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          Assets.images.logo.path,
+          // Sesuaikan dengan ukuran gambar Anda
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: MediaQuery.of(context).size.height * 0.5,
+          fit: BoxFit.contain, // Sesuaikan dengan kebutuhan Anda
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Text(
+          'By Fernanda Andyka Putra',
+          style: TextStyle(
+            color: AppColors.grey,
+            fontWeight: FontWeight.w600,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
