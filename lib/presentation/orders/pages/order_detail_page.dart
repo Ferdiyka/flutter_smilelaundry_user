ab365ff82ec4253f07ee0a8d4332aa6da1627c72
Andyka Putra <andykaputra219@gmail.com>
2024-04-07 20:47:27 +0700

    del samting

--- lib/presentation/orders/pages/order_detail_page.dart
+++ lib/presentation/orders/pages/order_detail_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_smilelaundry_user/core/extensions/int_ext.dart';
-import 'package:flutter_smilelaundry_user/presentation/address/pages/address_page.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../core/assets/assets.gen.dart';
